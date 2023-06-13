fc381acbbabda3efa73e9d15e28f179f1ec4a62c
frankjay <frankjr212711@gmail.com>
2023-06-13 09:13:09 +0000

    commit@master

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Syntax</title>
+</head>
+<body>
+
+ <!-- html comment -->
+    <?php 
+
+        echo "hello, world in a brackets";
+        echo ("Hello, world :) ");
+
+        // comments
+    ?>
+
+    <?php 
+        print('hello world <br>');
+        Print('Hello World <br>');
+        PRINT('HELLO WORLD <br><br>');
+    
+        // VARIABLES
+        $name = "Joe";
+        echo "hello " . $name . "<br>";
+        echo "hello " . $name . "<br>";
+        echo "hello " . $name . "<br>";
+
+        // BOOLEAN
+        $loggedIn = true;
+
+        // INTEGER
+        $myAge = 35
+ 
+    
+    ?>
+
+
+
+
+
+
+
+</body>
+</html>
