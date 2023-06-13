00f6328327d4a1e6f6749caba92836562def943f
frankjay <frankjr212711@gmail.com>
2023-06-13 09:37:29 +0000

    git_variables&contants

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
+<?php 
+    // CONSTANTS
+    define("TITLE", "PHP Varibles & Constants"); 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Syntax</title>
+    <title>PHP Syntax <?php echo TITLE; ?></title>
 </head>
 <body>
 
@@ -27,11 +31,30 @@
         echo "hello " . $name . "<br>";
         echo "hello " . $name . "<br>";
 
-        // BOOLEAN
+        // BOOLEAN: a boolean value specifies if a value is true or false
         $loggedIn = true;
+        $loggedOut = false;
 
-        // INTEGER
-        $myAge = 35
+        // INTEGER: is a number value
+        $myAge = 35;
+
+        // FLOATING POINT: Usually a fractional number with a decimal
+        $totalPrice = 136.56;
+
+        // STRING: Simple value that is enclosed by double quotations.
+
+        $fullName = "Brad Hussey";
+        $fullName = "John Doe";
+
+        // DISPLAY VARIABLES ON SCREEN
+        echo "Hello my name is $fullName and I am $myAge years old <br>";
+
+        // CONSTANTS
+        define("TITLE", "PHP Varibles & Constants "); 
+        echo TITLE;
+
+
+
  
     
     ?>
